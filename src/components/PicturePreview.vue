89d294e50c7f9601a418cc94f2a38af23b3cf030
mihauco <github@mihau.co>
2023-04-28 10:01:21 +0200

    bigger render size

--- src/components/PicturePreview.vue
+++ src/components/PicturePreview.vue
@@ -2,62 +2,88 @@
   <div
     class="picture-preview"
     @mousedown="mouseDownHandler"
+    :style="`width: ${previewSize}px; height: ${previewSize}px`"
   >
-    <svg width="278px" height="278px" view-box="0 0 278 278" ref="svg">
-      <defs>
-        <svg:style type="text/css">{{ latoBase64CSS }}</svg:style>
-        <clipPath id="clipImage">
-          <circle r="139" cx="139" cy="139"/>
-        </clipPath>
-        <path
-          id="textCircle"
-          d="
-            M 139 139
-            m 139 0
-            a 139,139 0 1,0 -278,0
-            a 139,139 0 1,0 278,0
-          "
-          transform="rotate(-45 139 139)"
+    <div
+      class="picture-preview__inner"
+      :style="`
+        width: ${renderSize}px;
+        transform: scale(${previewSize / renderSize});
+      `"
+    >
+      <svg
+        :width="`${renderSize}px`"
+        :height="`${renderSize}px`"
+        :view-box="`0 0 ${renderSize} ${renderSize}`"
+        ref="svg"
+      >
+        <defs>
+          <svg:style type="text/css">{{ latoBase64CSS }}</svg:style>
+          <clipPath id="clipImage">
+            <circle
+            :r="renderSize / 2"
+            :cx="renderSize / 2"
+            :cy="renderSize / 2"
+            />
+          </clipPath>
+          <path
+            id="textCircle"
+            :d="`
+              M ${renderSize / 2} ${renderSize / 2}
+              m ${renderSize / 2} 0
+              a ${renderSize / 2},${renderSize / 2} 0 1,0 -${renderSize},0
+              a ${renderSize / 2},${renderSize / 2} 0 1,0 ${renderSize},0
+            `"
+            :transform="`rotate(-45 ${renderSize / 2} ${renderSize / 2})`"
+          />
+          <linearGradient id="circleGradient" x1="1" x2="0" y1="0" y2="1" gradientTransform="rotate(0)">
+            <stop :offset="gradientOffset[0]" stop-color="black" stop-opacity="1"/>
+            <stop :offset="gradientOffset[1]" stop-color="white" stop-opacity="1"/>
+          </linearGradient>
+          <mask id="hole">
+            <rect
+              :width="`${renderSize}px`"
+              :height="`${renderSize}px`"
+              fill="url(#circleGradient)"
+            />
+            <circle
+              :r="Math.round(0.35 * renderSize)"
+              :cx="renderSize / 2"
+              :cy="renderSize / 2"
+            />
+          </mask>
+        </defs>
+        <image
+          :width="`${imageSvgSize.width}px`"
+          :height="`${imageSvgSize.height}px`"
+          :x="`${imageSvgPosition.x}px`"
+          :y="`${imageSvgPosition.y}px`"
+          :xlink:href="image"
+          clip-path="url(#clipImage)"
+          preserveAspectRatio="xMidYMid slice"
         />
-        <linearGradient id="circleGradient" x1="1" x2="0" y1="0" y2="1" gradientTransform="rotate(0)">
-          <stop :offset="gradientOffset[0]" stop-color="black" stop-opacity="1"/>
-          <stop :offset="gradientOffset[1]" stop-color="white" stop-opacity="1"/>
-        </linearGradient>
-        <mask id="hole">
-          <rect width="278px" height="278px" fill="url(#circleGradient)"/>
-          <circle r="97" cx="139" cy="139"/>
-        </mask>
-      </defs>
-      <image
-        :width="`${imageSvgSize.width}px`"
-        :height="`${imageSvgSize.height}px`"
-        :x="`${imageSvgPosition.x}px`"
-        :y="`${imageSvgPosition.y}px`"
-        :xlink:href="image"
-        clip-path="url(#clipImage)"
-        preserveAspectRatio="xMidYMid slice"
-      />
-      <circle
-        r="139"
-        cx="139"
-        cy="139"
-        :fill="backgroundColor"
-        mask="url(#hole)"
-      />
-      <text dy="-12" text-anchor="middle">
-        <textPath
-          xlink:href="#textCircle"
-          startOffset="50%"
-          :fill="textColor"
-          font-family="LatoB64, Arial"
-          font-weight="bold"
-          font-size="28px"
-          letter-spacing="5px"
-        >
-          {{ text }}
-        </textPath>
-      </text>
-    </svg>
+        <circle
+          :r="renderSize / 2"
+          :cx="renderSize / 2"
+          :cy="renderSize / 2"
+          :fill="backgroundColor"
+          mask="url(#hole)"
+        />
+        <text :dy="`-${Math.round(0.04 * renderSize)}`" text-anchor="middle">
+          <textPath
+            xlink:href="#textCircle"
+            startOffset="50%"
+            :fill="textColor"
+            font-family="LatoB64, Arial"
+            font-weight="bold"
+            :font-size="`${Math.round(0.1 * renderSize)}px`"
+            :letter-spacing="`${Math.round(0.018 * renderSize)}px`"
+          >
+            {{ text }}
+          </textPath>
+        </text>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -66,6 +92,7 @@ import { ref, computed, watch } from 'vue';
 import latoBase64CSS from '@/constants/latoBase64CSS';
 import inlineSvgToBase64Png from '@/helpers/inlineSvgToBase64Png';
 import getImageDimensionsFromBase64 from '@/helpers/getImageDimensionsFromBase64';
+import { renderSize, previewSize } from '@/constants/imageSize';
 
 const props = defineProps<{
   image: string
@@ -86,11 +113,11 @@ const imageSvgSize = computed(() => {
   const shorterSide: keyof typeof imageSize.value = imageSize.value.width < imageSize.value.height 
     ? 'width' : 'height';
   
-  const scale = (278 / imageSize.value[shorterSide]);
+  const scale = (renderSize / imageSize.value[shorterSide]);
 
   return {
-    width: shorterSide === 'width' ? 278 : imageSize.value.width * scale,
-    height: shorterSide === 'height' ? 278 : imageSize.value.height * scale
+    width: shorterSide === 'width' ? renderSize : imageSize.value.width * scale,
+    height: shorterSide === 'height' ? renderSize : imageSize.value.height * scale
   };
 });
 
@@ -136,8 +163,8 @@ const bodyMouseMoveHandler = (event: MouseEvent) => {
     const newXPos = imageSvgPosition.value.x + deltaX;
     const newYPos = imageSvgPosition.value.y + deltaY;
     
-    const maxX = imageSvgSize.value.width - 278;
-    const maxY = imageSvgSize.value.height - 278;
+    const maxX = imageSvgSize.value.width - renderSize;
+    const maxY = imageSvgSize.value.height - renderSize;
 
     imageSvgPosition.value = {
       x: deltaX === 0
@@ -161,8 +188,8 @@ const bodyMouseMoveHandler = (event: MouseEvent) => {
 
 watch(() => imageSvgSize.value, (newSize) => {
   imageSvgPosition.value = {
-    x: (278 - newSize.width) / 2,
-    y: (278 - newSize.height) / 2,
+    x: (renderSize - newSize.width) / 2,
+    y: (renderSize - newSize.height) / 2,
   }
 });
 
@@ -174,8 +201,13 @@ defineExpose({ getPngImage });
 
 <style lang="scss">
   .picture-preview {
-    width: 278px;
     margin: 0 auto;
+    overflow: hidden;
+    position: relative;
+
+    &__inner {
+      transform-origin: top left;
+    }
 
     svg {
       cursor: grab;
---
+++ src/constants/imageSize.ts
@@ -0,0 +1,2 @@
+export const renderSize = 390;
+export const previewSize = 278;
